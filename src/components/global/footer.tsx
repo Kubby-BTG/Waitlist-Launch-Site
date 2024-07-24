e36e9527a70f936b1e52a585d22fa670f3f4e1b1
thefathdev <abdillahfathu@gmail.com>
2024-07-24 23:05:24 +0700

    style: Footer component in desktop layout

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -94,6 +94,86 @@ export default async function Footer() {
               {settings.data.copyright}
             </p>
           </div>
+
+          {/* Desktop CTA and Nav */}
+          <div className="hidden grid-cols-3 gap-x-2 gap-y-14 md:grid">
+            <div className="flex flex-col gap-6">
+              <KubbyLogo />
+
+              <div className="flex gap-2">
+                {settings.data.social_media.map((item, i) => (
+                  <PrismicNextLink field={item.media_link} key={i}>
+                    <span className="sr-only">{item.media_name}</span>
+                    <PrismicNextImage field={item.media_icon} />
+                  </PrismicNextLink>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+              {settings.data.main_navigation.map((item, i) => (
+                <PrismicNextLink
+                  field={item.link}
+                  key={i}
+                  className={"text-sm text-white"}
+                >
+                  {item.label}
+                </PrismicNextLink>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <PrismicRichText
+                field={settings.data.cta_heading}
+                components={{
+                  heading2: ({ children }) => (
+                    <h2 className="font-display text-7xl font-extrabold uppercase leading-[4.25rem] text-white">
+                      {children}
+                    </h2>
+                  ),
+                  strong: ({ children }) => (
+                    <strong className="text-secondary">{children}</strong>
+                  ),
+                }}
+              />
+              <PrismicRichText
+                field={settings.data.cta_body}
+                components={{
+                  paragraph: ({ children }) => (
+                    <p className={"text-minimal text-sm leading-6"}>
+                      {children}
+                    </p>
+                  ),
+                }}
+              />
+
+              <div>
+                <Button
+                  variant={"accent"}
+                  className={"flex items-center gap-1"}
+                >
+                  <span>{settings.data.cta_button_text}</span>
+                  <Arrow className={"flex-none"} />
+                </Button>
+              </div>
+            </div>
+
+            <div className="col-span-2 flex gap-6">
+              {settings.data.secondary_navigation.map((item, i) => (
+                <PrismicNextLink
+                  field={item.link}
+                  key={i}
+                  className={"text-sm text-white"}
+                >
+                  {item.label}
+                </PrismicNextLink>
+              ))}
+            </div>
+
+            <p className={"text-xs leading-5 text-white"}>
+              {settings.data.copyright}
+            </p>
+          </div>
         </div>
       </div>
     </footer>
