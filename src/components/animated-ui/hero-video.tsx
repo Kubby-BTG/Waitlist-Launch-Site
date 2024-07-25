14242ad427a376b279eaf1c8e05b880063ef3650
thefathdev <abdillahfathu@gmail.com>
2024-07-26 02:36:38 +0700

    style: Update play icon

--- src/components/animated-ui/hero-video.tsx
+++ src/components/animated-ui/hero-video.tsx
@@ -31,7 +31,7 @@ export default function HeroVideo() {
         }
         onClick={togglePlayPause}
       >
-        {isPlaying ? <PauseIcon /> : "Play"}
+        {isPlaying ? <PauseIcon /> : <PlayIcon />}
       </button>
     </div>
   );
@@ -55,6 +55,23 @@ const PauseIcon = () => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M10.8334 3.33333C10.8334 2.8731 11.2065 2.5 11.6667 2.5H15C15.4603 2.5 15.8334 2.8731 15.8334 3.33333V16.6667C15.8334 17.1269 15.4603 17.5 15 17.5H11.6667C11.2065 17.5 10.8334 17.1269 10.8334 16.6667V3.33333ZM12.5 4.16667V15.8333H14.1667V4.16667H12.5Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+const PlayIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3.76738 1.76859C4.03495 1.62252 4.36091 1.63419 4.61734 1.79904L16.284 9.29904C16.5225 9.45237 16.6667 9.71647 16.6667 10C16.6667 10.2836 16.5225 10.5477 16.284 10.701L4.61734 18.201C4.36091 18.3658 4.03495 18.3775 3.76738 18.2314C3.49982 18.0854 3.33337 17.8049 3.33337 17.5V2.50002C3.33337 2.19518 3.49982 1.91467 3.76738 1.76859ZM5.00004 4.02641V15.9736L14.2923 10L5.00004 4.02641Z"
       fill="currentColor"
     />
   </svg>
