323d409427d101f1a4cdd7a1dc570d86498cbc68
thefathdev <abdillahfathu@gmail.com>
2024-07-26 02:51:40 +0700

    feat: Add heading and body text to partnership page

--- src/slices/PartnershipForm/index.tsx
+++ src/slices/PartnershipForm/index.tsx
@@ -1,5 +1,5 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `PartnershipForm`.
@@ -15,9 +15,35 @@ const PartnershipForm = ({ slice }: PartnershipFormProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={
+        "bg-primary pb-32 pt-10 md:flex md:min-h-[calc(90vh-124px)] md:items-center md:py-10"
+      }
     >
-      Placeholder component for partnership_form (variation: {slice.variation})
-      Slices
+      <div className="container flex grid-cols-2 flex-col gap-8 md:grid md:items-center md:gap-0">
+        <div className="flex flex-col gap-4 md:gap-6">
+          <PrismicRichText
+            field={slice.primary.heading}
+            components={{
+              heading1: ({ children }) => (
+                <h1 className="font-display text-[3.25rem] font-extrabold uppercase leading-[3.5rem] text-white md:text-[6rem] md:leading-[5.5rem]">
+                  {children}
+                </h1>
+              ),
+              strong: ({ children }) => (
+                <strong className="text-secondary">{children}</strong>
+              ),
+            }}
+          />
+          <PrismicRichText
+            field={slice.primary.body}
+            components={{
+              paragraph: ({ children }) => (
+                <p className="text-base text-white">{children}</p>
+              ),
+            }}
+          />
+        </div>
+      </div>
     </section>
   );
 };
