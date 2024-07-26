206b1271b0e6272e0691864e6bfda2d2f87398c6
thefathdev <abdillahfathu@gmail.com>
2024-07-26 21:49:22 +0700

    chore: Remove unused import

--- src/components/blog-post/post-card.tsx
+++ src/components/blog-post/post-card.tsx
@@ -1,7 +1,7 @@
 import { Content } from "@prismicio/client";
 import Arrow from "../ui/arrow";
 import { Badge } from "../ui/badge";
-import { PrismicLink, PrismicRichText } from "@prismicio/react";
+import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { formatDate } from "@/lib/utils";
 
@@ -10,7 +10,7 @@ export default function PostCard({ post }: { post: Content.BlogPostDocument }) {
     <PrismicNextLink
       document={post}
       className={
-        "hover:shadow-expand ease-kubby group flex w-full flex-col gap-4 rounded-lg bg-background-secondary p-4 pb-6 shadow-none transition-all duration-500 hover:bg-white/80"
+        "group flex w-full flex-col gap-4 rounded-lg bg-background-secondary p-4 pb-6 shadow-none transition-all duration-500 ease-kubby hover:bg-white/80 hover:shadow-expand"
       }
     >
       <div className="relative flex h-[12.5rem] items-center justify-center overflow-hidden rounded-[5px] bg-input">
@@ -24,7 +24,7 @@ export default function PostCard({ post }: { post: Content.BlogPostDocument }) {
         <div className="flex items-center justify-between">
           <p className={"text-xs uppercase leading-6"}>{post.data.category}</p>
 
-          <div className="ease-kubby flex size-8 items-center justify-center rounded-full border border-input text-black transition-all duration-500 group-hover:border-transparent group-hover:bg-primary group-hover:text-white">
+          <div className="flex size-8 items-center justify-center rounded-full border border-input text-black transition-all duration-500 ease-kubby group-hover:border-transparent group-hover:bg-primary group-hover:text-white">
             <Arrow className={"size-4"} />
           </div>
         </div>
@@ -48,7 +48,7 @@ export default function PostCard({ post }: { post: Content.BlogPostDocument }) {
             }}
           />
 
-          <span className="text-gray flex gap-1 text-[11px] leading-5">
+          <span className="flex gap-1 text-[11px] leading-5 text-gray">
             <span>{post.data.author}</span>|
             <span>
               {post.data.publication_date
