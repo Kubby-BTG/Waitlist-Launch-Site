71c742bb3d024263c9dcf4aff9e924a39bbae64d
thefathdev <abdillahfathu@gmail.com>
2024-07-26 23:52:31 +0700

    feat: Add Heading and CuratedPosts

--- src/slices/FilteredBlogCollections/index.tsx
+++ src/slices/FilteredBlogCollections/index.tsx
@@ -1,5 +1,6 @@
+import CuratedPosts from "@/components/blog-post/curated-posts";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `FilteredBlogCollections`.
@@ -17,9 +18,25 @@ const FilteredBlogCollections = ({
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={"bg-background py-16 md:py-32"}
     >
-      Placeholder component for filtered_blog_collections (variation:{" "}
-      {slice.variation}) Slices
+      <div className="container flex flex-col gap-10">
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={{
+            heading2: ({ children }) => (
+              <h2 className="font-display text-[4rem] font-extrabold uppercase leading-[3.5rem] text-primary md:text-[5rem] md:leading-[3.5rem]">
+                {children}
+              </h2>
+            ),
+            strong: ({ children }) => (
+              <strong className="text-brand">{children}</strong>
+            ),
+          }}
+        />
+
+        <CuratedPosts limit={6} />
+      </div>
     </section>
   );
 };
