83149ad32bc37718a00d117ce03eb4f950742f0f
thefathdev <abdillahfathu@gmail.com>
2024-07-29 03:04:15 +0700

    style: Update max width

--- src/slices/ContactForm/index.tsx
+++ src/slices/ContactForm/index.tsx
@@ -22,15 +22,17 @@ const ContactForm = ({ slice }: ContactFormProps): JSX.Element => {
       data-slice-variation={slice.variation}
       className={"bg-background py-16 md:py-32"}
     >
-      <div className="container mx-auto flex max-w-[29rem] flex-col gap-8 md:gap-10">
-        <div className="flex flex-col gap-4 md:items-center md:gap-6 md:text-center">
+      <div className="container mx-auto flex flex-col gap-8 md:gap-10">
+        <div className="mx-auto flex max-w-[36rem] flex-col gap-4 md:items-center md:gap-6 md:text-center">
           <h1 className={"heading-2 text-primary"}>
             <DoubleSlideUpText>{slice.primary.heading}</DoubleSlideUpText>
           </h1>
           <p className="body-2 text-primary">{slice.primary.body}</p>
         </div>
 
-        <ContactUsForm />
+        <div className={"mx-auto w-full max-w-[29rem]"}>
+          <ContactUsForm />
+        </div>
       </div>
     </section>
   );
