56562ac0fb2134ea9d8eb4eba1dd6feddf365c54
thefathdev <abdillahfathu@gmail.com>
2024-07-28 23:47:11 +0700

    stye: Update heading font size in mobile

--- src/components/global/footer.tsx
+++ src/components/global/footer.tsx
@@ -27,7 +27,7 @@ export default async function Footer() {
               field={settings.data.cta_heading}
               components={{
                 heading2: ({ children }) => (
-                  <h2 className="font-display text-[4.25rem] font-extrabold uppercase leading-[4.5rem] text-white">
+                  <h2 className="heading-3 text-balance text-white">
                     {children}
                   </h2>
                 ),
