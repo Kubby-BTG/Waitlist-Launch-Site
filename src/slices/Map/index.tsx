f6db2b17d242842df9b300978b43f2b601e2ef41
thefathdev <abdillahfathu@gmail.com>
2024-07-29 23:14:27 +0700

    feat: Add google map

--- src/slices/Map/index.tsx
+++ src/slices/Map/index.tsx
@@ -14,8 +14,26 @@ const Map = ({ slice }: MapProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className={"relative bg-background py-16"}
     >
-      Placeholder component for map (variation: {slice.variation}) Slices
+      <div className="absolute inset-0 translate-y-1/2 bg-primary"></div>
+      <div className="container">
+        <div
+          className={
+            "relative h-[600px] w-full overflow-hidden md:h-[640px] md:rounded-2xl"
+          }
+        >
+          <iframe
+            // width="450"
+            // height="250"
+            className={"h-full w-full"}
+            frameBorder="0"
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCsMCSpSDnkL8mxz18R2XI_BU31cZTvjyc&q=Eiffel+Tower,Paris+France"
+            allowFullScreen
+          />
+        </div>
+      </div>
     </section>
   );
 };
