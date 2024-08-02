3137c19bf8965b5a1df56834278376f0cfa67d10
Chris Uzoh <genbliz@gmail.com>
2024-08-02 15:36:48 +0100

    fixes for modal

--- src/components/ui/dialog-custom.tsx
+++ src/components/ui/dialog-custom.tsx
@@ -1,67 +1,121 @@
-// import React from "react";
+import React from "react";
+import { X } from "lucide-react";
+import { IPropsWithChildren } from "../../types";
+import { cn } from "@/lib/utils";
+import MountModalPortal from "../helpers/MountModalPortal";
 
-// export default function Modal() {
-//   const [showModal, setShowModal] = React.useState(false);
-//   return (
-//     <>
-//       <button
-//         className="bg-pink-500 active:bg-pink-600 shadow hover:shadow-lg mb-1 mr-1 rounded px-6 py-3 text-sm font-bold uppercase text-white outline-none transition-all duration-150 ease-linear focus:outline-none"
-//         type="button"
-//         onClick={() => setShowModal(true)}
-//       >
-//         Open regular modal
-//       </button>
-//       {showModal ? (
-//         <>
-//           <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
-//             <div className="relative mx-auto my-6 w-auto max-w-3xl">
-//               {/*content*/}
-//               <div className="shadow-lg relative flex w-full flex-col rounded-lg border-0 bg-white outline-none focus:outline-none">
-//                 {/*header*/}
-//                 <div className="border-blueGray-200 flex items-start justify-between rounded-t border-b border-solid p-5">
-//                   <h3 className="text-3xl font-semibold">Modal Title</h3>
-//                   <button
-//                     className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black opacity-5 outline-none focus:outline-none"
-//                     onClick={() => setShowModal(false)}
-//                   >
-//                     <span className="block h-6 w-6 bg-transparent text-2xl text-black opacity-5 outline-none focus:outline-none">
-//                       ×
-//                     </span>
-//                   </button>
-//                 </div>
-//                 {/*body*/}
-//                 <div className="relative flex-auto p-6">
-//                   <p className="text-blueGray-500 my-4 text-lg leading-relaxed">
-//                     I always felt like I could do anything. That is the main
-//                     thing people are controlled by! Thoughts- their perception
-//                     of themselves! They are slowed down by their perception of
-//                     themselves. If you are taught you cannot do anything, you
-//                     wont do anything. I was taught I could do everything.
-//                   </p>
-//                 </div>
-//                 {/*footer*/}
-//                 <div className="border-blueGray-200 flex items-center justify-end rounded-b border-t border-solid p-6">
-//                   <button
-//                     className="text-red-500 background-transparent mb-1 mr-1 px-6 py-2 text-sm font-bold uppercase outline-none transition-all duration-150 ease-linear focus:outline-none"
-//                     type="button"
-//                     onClick={() => setShowModal(false)}
-//                   >
-//                     Close
-//                   </button>
-//                   <button
-//                     className="bg-emerald-500 active:bg-emerald-600 shadow hover:shadow-lg mb-1 mr-1 rounded px-6 py-3 text-sm font-bold uppercase text-white outline-none transition-all duration-150 ease-linear focus:outline-none"
-//                     type="button"
-//                     onClick={() => setShowModal(false)}
-//                   >
-//                     Save Changes
-//                   </button>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//           <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-//         </>
-//       ) : null}
-//     </>
-//   );
-// }
+type IProps = IPropsWithChildren & {
+  className?: string;
+  isOpen: boolean;
+};
+
+type IPropChild = IPropsWithChildren & {
+  className?: string;
+};
+
+const AppModalDialog = ({ className, isOpen, children }: IProps) => {
+  if (!isOpen) {
+    return null;
+  }
+
+  return (
+    <MountModalPortal>
+      <div
+        className={cn([
+          "fixed inset-0 z-50 flex items-center justify-center",
+          "overflow-y-auto overflow-x-hidden outline-none focus:outline-none",
+        ])}
+      >
+        <div
+          className={cn(["relative mx-auto my-6 w-auto max-w-3xl", className])}
+        >
+          {children}
+        </div>
+      </div>
+      <div className={cn(["fixed inset-0 z-40 bg-black/50"])}></div>
+    </MountModalPortal>
+  );
+};
+
+AppModalDialog.Content = ({ className, children }: IPropChild) => {
+  return (
+    <div
+      className={cn([
+        //
+        "shadow-lg fixed left-[50%] top-[50%] z-50 grid w-[calc(100vw-3rem)] max-w-md",
+        "translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg",
+        "border border-minimal bg-white p-6 duration-200",
+        "",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0",
+        "data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
+        "data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]",
+        "data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+        "",
+        "md:w-full md:max-w-[43.5rem]",
+        className,
+      ])}
+    >
+      {children}
+    </div>
+  );
+};
+
+AppModalDialog.Header = ({ className, children }: IPropChild) => {
+  return (
+    <div
+      className={cn([
+        "flex flex-col space-y-1.5 text-center sm:text-left",
+        className,
+      ])}
+    >
+      {children}
+    </div>
+  );
+};
+
+AppModalDialog.HeaderTitle = ({ className, children }: IProps) => {
+  return <div className={cn([])}>{children}</div>;
+};
+
+AppModalDialog.CloseButton = ({
+  className,
+  children,
+  handleClick,
+}: IPropsWithChildren & {
+  className?: string;
+  handleClick: () => void;
+}) => {
+  return (
+    <button
+      onClick={() => handleClick()}
+      type={"button"}
+      className={cn([
+        "flex size-8 flex-none items-center justify-center rounded-full bg-input-secondary text-black",
+      ])}
+    >
+      <span className="sr-only">Close</span>
+      <X className={"size-4"} />
+    </button>
+  );
+};
+
+AppModalDialog.Body = ({ className, children }: IPropChild) => {
+  return (
+    <div className={cn(["relative flex-auto p-6", className])}>{children}</div>
+  );
+};
+
+AppModalDialog.Footer = ({ className, children }: IPropChild) => {
+  return (
+    <div
+      className={cn([
+        "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+        className,
+      ])}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default AppModalDialog;
