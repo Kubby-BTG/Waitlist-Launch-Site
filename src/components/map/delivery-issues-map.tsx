bd2c25eed415e8996810011cca3eee8e038cac36
Chris Uzoh <genbliz@gmail.com>
2024-08-07 15:55:05 +0100

    added animation to map

--- src/components/map/delivery-issues-map.tsx
+++ src/components/map/delivery-issues-map.tsx
@@ -2,10 +2,10 @@
 
 import FilterIssuesForm, { IFilterIssueParams } from "../modals/filter-issues-form";
 import { Button } from "../ui/button";
-import { AdvancedMarker, Map } from "@vis.gl/react-google-maps";
+import { AdvancedMarker, Map, useMap } from "@vis.gl/react-google-maps";
 
 import FilterIcon from "./filter-icon";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import PinIcon from "./pin-icon";
 import ShowDeliveryNearYouForm from "../modals/show-delivery-near-you-form";
 
@@ -15,7 +15,9 @@ import useAppFormPost from "@/hooks/useAppFormPost";
 import { IDeliveryIssue } from "../../airtable/types";
 import AppAlertDialog, { useAppAlertDialog } from "../ui/AppAlertDialog";
 import { GoogleMapService } from "../../utils/google-map-service";
-import LoadGoogleMapProvider from "./load-map";
+import LoadGoogleMapProvider from "./load-google-map";
+import { UtilService } from "../../utils/util-service";
+import LoadGoogleMapGeocoding from "./load-googlemap-geocoding";
 
 type IssueInfo = {
   count: number | null;
@@ -38,42 +40,55 @@ const centerPoints = {
 };
 
 const CURRENT_GOOGLE_MAP_ID = "19cefe5f097a79a6_tn";
+const CURRENT_GOOGLE_MAP_ID_MIAN = "a615f09cefe7997a_main";
 
 export default function DeliveryIssuesMap() {
   const [isShowFilterForm, setIsShowFilterForm] = useState(false);
-  const [isMapEnabled, setIsMapEnabled] = useState(true);
   const [isShowShowDeliveryIssuesForm, setIsShowShowDeliveryIssues] = useState(false);
   const [deliveryIssue, setDeliveryIssue] = useState<IssueInfo>({ ...initialData });
   const { alertOptions, isAlertOpen, closeAlertDialog, openAlertDialog } = useAppAlertDialog();
   const { postData, isBusy } = useAppFormPost();
-  // const map = useMap();
-  const [userZoom, setUserZoom] = useState(zoomLevels.USA);
-  const [center, setCenter] = useState(centerPoints.USA);
-  // const geocodingLib = useMapsLibrary("geocoding");
-  // const geocoder = useMemo(() => geocodingLib && new geocodingLib.Geocoder(), [geocodingLib]);
+  const [zoom, setZoom1] = useState(zoomLevels.USA);
+  const [center, setCenter1] = useState(centerPoints.USA);
+  const map = useMap(CURRENT_GOOGLE_MAP_ID_MIAN);
 
   useEffect(() => {
     // getAllDeliveryIssues().catch(() => {});
   }, []);
 
-  function centerToUsa() {
-    if (userZoom !== zoomLevels.USA || center.lat !== centerPoints.USA.lat) {
-      setUserZoom(zoomLevels.USA);
-      setCenter(centerPoints.USA);
-      restartMap();
+  useEffect(() => {
+    // console.log({ map });
+  }, [map]);
+
+  function setZoom(zoom01: number) {
+    if (map?.setZoom) {
+      map.setZoom(zoom01);
+      setZoom1(zoom01);
     }
   }
 
-  function restartMap() {
-    setIsMapEnabled(false);
-    setTimeout(() => {
-      setIsMapEnabled(true);
-    }, 500);
+  function setCenter(latLng: { lat: number; lng: number }) {
+    if (map?.panTo) {
+      map.panTo(latLng);
+      setCenter1(latLng);
+    }
+  }
+
+  function centerToUsa() {
+    if (zoom !== zoomLevels.USA || center.lat !== centerPoints.USA.lat) {
+      setZoom(zoomLevels.USA);
+      setCenter(centerPoints.USA);
+    }
   }
 
   function getDeliveryCompanyByName(name: string | undefined) {
     if (!name) return undefined;
     return deliveryCompanies.find((f) => f?.name?.toLowerCase() === name?.toLowerCase());
+  }
+
+  function closeModals() {
+    setIsShowFilterForm(false);
+    setIsShowShowDeliveryIssues(false);
   }
 
   async function handleFindByManyParams(params: Partial<IFilterIssueParams>) {
@@ -136,9 +151,10 @@ export default function DeliveryIssuesMap() {
           const results02 = GoogleMapService.getFirtstLocation(result01);
 
           if (results02) {
-            setUserZoom(zoomLevels.DEFAULT);
+            closeModals();
             setCenter(results02);
-            restartMap();
+            await UtilService.waitUntilMilliseconds(2000);
+            setZoom(zoomLevels.DEFAULT);
           }
         } else if (params01.zipcode) {
           const result01 = await GoogleMapService.getGeocodeAddressByZipcode(params01.zipcode);
@@ -146,9 +162,10 @@ export default function DeliveryIssuesMap() {
           const results02 = GoogleMapService.getFirtstLocation(result01);
 
           if (results02) {
-            setUserZoom(zoomLevels.DEFAULT);
+            closeModals();
             setCenter(results02);
-            restartMap();
+            await UtilService.waitUntilMilliseconds(2000);
+            setZoom(zoomLevels.DEFAULT);
           }
         } else {
           centerToUsa();
@@ -159,16 +176,10 @@ export default function DeliveryIssuesMap() {
 
       setDeliveryIssue(options01);
 
-      openAlertDialog.info({
-        title: `${apiData.length} issue(s) found`,
-        // description: apiData.length ? "Note: Map location mark not implimented yet" : undefined,
-      });
+      // openAlertDialog.info({ title: `${apiData.length} issue(s) found` });
 
-      setIsShowFilterForm(false);
-      setIsShowShowDeliveryIssues(false);
+      closeModals();
     } catch (error) {
-      console.error(error);
-      console.log({ error });
       openAlertDialog.error({ title: "Could not filter. Error occured" });
     }
   }
@@ -191,65 +202,60 @@ export default function DeliveryIssuesMap() {
   }
 
   return (
-    <LoadGoogleMapProvider>
+    <>
       <div className={"relative h-[600px] w-full overflow-hidden md:h-[640px] md:rounded-2xl"}>
-        {isMapEnabled && (
+        <Map
+          defaultZoom={zoomLevels.USA}
+          defaultCenter={centerPoints.USA}
+          disableDefaultUI={true}
+          mapId={CURRENT_GOOGLE_MAP_ID}
+          // gestureHandling={"greedy"}
+          id={CURRENT_GOOGLE_MAP_ID_MIAN}
+        >
           <>
-            <Map
-              defaultZoom={userZoom}
-              defaultCenter={center}
-              disableDefaultUI={true}
-              mapId={CURRENT_GOOGLE_MAP_ID}
-              //   onCameraChanged={ (ev: MapCameraChangedEvent) =>
-              //     console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-              //   }
-            >
-              <>
-                {/* <Marker position={{ lat: 32.9618763, lng: -96.99609249999999 }} /> */}
+            {/* <Marker position={{ lat: 32.9618763, lng: -96.99609249999999 }} /> */}
 
-                {/* <AdvancedMarker position={center}>
+            {/* <AdvancedMarker position={center}>
               <img src={deliveryIssue.carrierLogo || "/markers/cube.svg"} alt="" className={"size-8"} />
             </AdvancedMarker> */}
 
-                {deliveryIssue.issue && (
-                  <AdvancedMarker position={center}>
-                    <HoverCard open={true}>
-                      <HoverCardTrigger>
-                        <img src={deliveryIssue.carrierLogo || "/markers/cube.svg"} alt="" className={"size-8"} />
-                      </HoverCardTrigger>
-                      <HoverCardContent
-                        side={"top"}
-                        sideOffset={12}
-                        forceMount={true}
-                        className="flex h-fit w-fit flex-col items-center gap-2 rounded-lg border-none bg-black px-3 py-3 font-sans text-white"
-                      >
-                        <div className="absolute bottom-0 translate-y-[80%]">
-                          <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path
-                              d="M8.73649 7.96115C7.9687 9.30478 6.0313 9.30478 5.26351 7.96115L0.709874 -0.00772253C-0.0520192 -1.34104 0.910715 -3 2.44636 -3L11.5536 -3C13.0893 -3 14.052 -1.34103 13.2901 -0.0077215L8.73649 7.96115Z"
-                              fill="black"
-                            />
-                          </svg>
-                        </div>
-                        <div className="text-sm">
-                          {deliveryIssue.count && <span className="font-bold"> {deliveryIssue.count} - </span>}
-                          {deliveryIssue.issue && <span className="font-bold"> {deliveryIssue.issue}</span>}
-                        </div>
-                        {deliveryIssue.location && (
-                          <div className="flex items-center gap-1 rounded-lg bg-[#2F3233] px-2 py-1 text-sm text-white">
-                            <PinIcon />
-                            <span>{deliveryIssue.location}</span>
-                          </div>
-                        )}
-                      </HoverCardContent>
-                    </HoverCard>
-                  </AdvancedMarker>
-                )}
-              </>
-            </Map>
+            {deliveryIssue.issue && (
+              <AdvancedMarker position={center}>
+                <HoverCard open={true}>
+                  <HoverCardTrigger>
+                    <img src={deliveryIssue.carrierLogo || "/markers/cube.svg"} alt="" className={"size-8"} />
+                  </HoverCardTrigger>
+                  <HoverCardContent
+                    side={"top"}
+                    sideOffset={12}
+                    forceMount={true}
+                    className="flex h-fit w-fit flex-col items-center gap-2 rounded-lg border-none bg-black px-3 py-3 font-sans text-white"
+                  >
+                    <div className="absolute bottom-0 translate-y-[80%]">
+                      <svg width="14" height="9" viewBox="0 0 14 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          d="M8.73649 7.96115C7.9687 9.30478 6.0313 9.30478 5.26351 7.96115L0.709874 -0.00772253C-0.0520192 -1.34104 0.910715 -3 2.44636 -3L11.5536 -3C13.0893 -3 14.052 -1.34103 13.2901 -0.0077215L8.73649 7.96115Z"
+                          fill="black"
+                        />
+                      </svg>
+                    </div>
+                    <div className="text-sm">
+                      {deliveryIssue.count && <span className="font-bold"> {deliveryIssue.count} - </span>}
+                      {deliveryIssue.issue && <span className="font-bold"> {deliveryIssue.issue}</span>}
+                    </div>
+                    {deliveryIssue.location && (
+                      <div className="flex items-center gap-1 rounded-lg bg-[#2F3233] px-2 py-1 text-sm text-white">
+                        <PinIcon />
+                        <span>{deliveryIssue.location}</span>
+                      </div>
+                    )}
+                  </HoverCardContent>
+                </HoverCard>
+              </AdvancedMarker>
+            )}
           </>
-        )}
-
+        </Map>
+        {/* <LoadGoogleMapGeocoding /> */}
         <div className="absolute left-6 top-8 flex flex-wrap gap-2 md:gap-4">
           <Button
             className={"gap-1 rounded-full bg-background-icon text-sm font-normal hover:bg-background-icon/80"}
@@ -291,6 +297,6 @@ export default function DeliveryIssuesMap() {
 
         <AppAlertDialog handleCancel={() => closeAlertDialog()} open={isAlertOpen} config={alertOptions} />
       </div>
-    </LoadGoogleMapProvider>
+    </>
   );
 }
---
+++ src/components/map/load-googlemap-geocoding.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+// import { useMapsLibrary } from "@vis.gl/react-google-maps";
+// import { useEffect, useMemo } from "react";
+
+const LoadGoogleMapGeocoding = () => {
+  // const geocodingLib = useMapsLibrary("geocoding");
+  // const geocoder = useMemo(() => geocodingLib && new geocodingLib.Geocoder(), [geocodingLib]);
+
+  // useEffect(() => {
+  //   if (!geocoder) return;
+
+  //   console.log({ geocoder });
+
+  // }, [geocoder]);
+
+  return <></>;
+};
+
+export default LoadGoogleMapGeocoding;
