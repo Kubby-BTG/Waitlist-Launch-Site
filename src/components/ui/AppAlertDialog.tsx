7c0cc1ea1a5b600809576371859f13cf8a706c4b
Chris Uzoh <genbliz@gmail.com>
2024-08-09 15:35:15 +0100

    updated dialog width

--- src/components/ui/AppAlertDialog.tsx
+++ src/components/ui/AppAlertDialog.tsx
@@ -33,8 +33,9 @@ const AlertDialogContent = forwardRef<
       ref={ref}
       className={cn([
         "border-brand-200 shadow-lg dark:border-brand-800 dark:bg-brand-950 fixed left-[50%] top-[50%]",
-        "z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-white p-6",
+        "z-50 grid max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-white p-6",
         "duration-200",
+        "w-[calc(100%-15px)] md:ml-0 md:mr-0 md:w-full",
         "data-[state=open]:animate-in data-[state=closed]:animate-out",
         "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95",
         "data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2",
