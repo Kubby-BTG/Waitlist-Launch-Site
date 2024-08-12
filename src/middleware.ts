abd9b4f69d54c4dc3bb15c797e99d6bf590cd7bc
Chris Uzoh <genbliz@gmail.com>
2024-08-12 10:37:44 +0100

    added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,84 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { DeliveryIssuesApiService } from "./airtable/tables/delivery-issue";
+import { IQueryParameters, IDeliveryIssue } from "./airtable/types";
+import { ContactApiService } from "./airtable/tables/contact";
+import { PartnersApiService } from "./airtable/tables/partner";
+import { revalidateTag } from "next/cache";
+
+import { exitPreview, redirectToPreviewURL } from "@prismicio/next";
+import { createClient } from "./prismicio";
+
+const routesMonitor = {
+  DeliveryIssueCreate: "/api/delivery-issue",
+  DeliveryIssue_Find: "/api/delivery-issue/find",
+  Contact: "/api/contact",
+  Waitlist: "/api/waitlist",
+  Partner: "/api/partner",
+  Revalidate: "/api/revalidate",
+  ExitPreview: "/api/exit-preview",
+  Preview: "/api/preview",
+};
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const pathname = new URL(request.url).pathname;
+
+  console.log({ url: request.url, pathname });
+
+  if (request.method.toUpperCase() === "POST") {
+    if (pathname === routesMonitor.DeliveryIssueCreate) {
+      const recordData = await request.json();
+
+      const result = await DeliveryIssuesApiService.createRecordBase({ recordData });
+      return NextResponse.json(result);
+    }
+
+    if (pathname === routesMonitor.DeliveryIssue_Find) {
+      const query = (await request.json()) as IQueryParameters<IDeliveryIssue>;
+
+      const result = await DeliveryIssuesApiService.findRecordBase({ query });
+      return NextResponse.json(result);
+    }
+
+    if (pathname === routesMonitor.Contact) {
+      const recordData = await request.json();
+
+      const result = await ContactApiService.createRecordBase({ recordData });
+
+      return NextResponse.json(result);
+    }
+
+    if (pathname === routesMonitor.Partner) {
+      const recordData = await request.json();
+
+      const result = await PartnersApiService.createRecordBase({ recordData });
+      return NextResponse.json(result);
+    }
+
+    if (pathname === routesMonitor.Revalidate) {
+      revalidateTag("prismic");
+
+      return NextResponse.json({ revalidated: true, now: Date.now() });
+    }
+  }
+
+  if (request.method.toUpperCase() === "GET") {
+    if (pathname === routesMonitor.Preview) {
+      const client = createClient();
+
+      return await redirectToPreviewURL({ client, request });
+    }
+
+    if (pathname === routesMonitor.ExitPreview) {
+      return exitPreview();
+    }
+  }
+
+  return NextResponse.json({ invalid_route: true });
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/api/:path*"],
+};
