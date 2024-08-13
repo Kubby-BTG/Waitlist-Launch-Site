3bbf2d75fcf39a98a7eaa540628f16c78b7ecde8
Chris Uzoh <genbliz@gmail.com>
2024-08-13 09:14:43 +0100

    updated robot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: ["/private/", "/slice-simulator/"],
     },
     sitemap: [AppConfig().CURRENT_SITE_URL, "sitemap.xml"].join("/"),
   };
