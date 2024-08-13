e7cc81f85361aefd95d6ee7821ad4d42fb395f9f
Chris Uzoh <genbliz@gmail.com>
2024-08-13 08:46:05 +0100

    added seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Header from "@/components/global/header";
 import Footer from "@/components/global/footer";
 import AnimatedLayout from "@/components/animated-ui/animated-layout";
 import ExitAnimationWrapper from "@/components/animated-ui/exit-animation-wrapper";
+import { AppConfig, AppDescription, ApplicationAuthor, ApplicationKeyword, ApplicationSiteName } from "../utils/constants";
 
 const DMSans = DM_Sans({ subsets: ["latin"], variable: "--font-dm-sans" });
 
@@ -17,20 +18,93 @@ const TTBluescreens = localFont({
   variable: "--font-tt-bluescreens",
 });
 
-// export const revalidate = 3;
+const metadataData: Metadata = {
+  title: {
+    template: `%s | ${ApplicationSiteName}`,
+    default: "Kubby",
+  },
+  metadataBase: new URL(AppConfig().CURRENT_SITE_URL),
+  keywords: Array.from(new Set(ApplicationKeyword)),
+  // alternates: {
+  //   canonical: "/",
+  //   languages: {
+  //     "en-US": "/en-US",
+  //     "de-DE": "/de-DE",
+  //   },
+  // },
+  twitter: {
+    card: "summary_large_image",
+    description: AppDescription,
+    creator: ApplicationAuthor,
+    images: "/twitter-image.png",
+    title: ApplicationSiteName,
+  },
+  icons: {
+    icon: "/icon.png",
+    apple: "/apple-icon.png",
+  },
+  openGraph: {
+    images: "/opengraph-image.png",
+    type: "website",
+
+    siteName: ApplicationSiteName,
+    title: ApplicationSiteName,
+    description: AppDescription,
+    // phoneNumbers: AppConfig.CONTACT_PHONE_NUMBER_01,
+    // emails: AppConfig.CONTACT_EMAIL,
+  },
+};
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
 
   const settings = await client.getSingle("settings");
 
+  // return {
+  //   ...metadataData,
+  //   title: settings.data.site_title || metadataData.title,
+  //   description: settings.data.meta_description || "Kubby description",
+  //   openGraph: {
+  //     images: [settings.data.og_image.url || ""],
+  //   },
+  // };
+
   return {
-    title: settings.data.site_title || "Kubby",
-    description: settings.data.meta_description || "Kubby description",
-    openGraph: {
-      images: [settings.data.og_image.url || ""],
+    title: {
+      template: `%s | ${ApplicationSiteName}`,
+      default: "Kubby",
     },
-  };
+    metadataBase: new URL(AppConfig().CURRENT_SITE_URL),
+    keywords: Array.from(new Set(ApplicationKeyword)),
+    // alternates: {
+    //   canonical: "/",
+    //   languages: {
+    //     "en-US": "/en-US",
+    //     "de-DE": "/de-DE",
+    //   },
+    // },
+    twitter: {
+      card: "summary_large_image",
+      description: AppDescription,
+      creator: ApplicationAuthor,
+      images: "/twitter-image.png",
+      title: ApplicationSiteName,
+    },
+    icons: {
+      icon: "/icon.png",
+      apple: "/apple-icon.png",
+    },
+    openGraph: {
+      images: "/opengraph-image.png",
+      type: "website",
+
+      siteName: ApplicationSiteName,
+      title: ApplicationSiteName,
+      description: AppDescription,
+      // phoneNumbers: AppConfig.CONTACT_PHONE_NUMBER_01,
+      // emails: AppConfig.CONTACT_EMAIL,
+    },
+  } satisfies Metadata;
 }
 
 export default function RootLayout({
