9ef39f78e9508dcf37eb1303352f33b8e8e639a3
Chris Uzoh <genbliz@gmail.com>
2024-08-15 13:45:51 +0100

    updated home title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { SliceZone } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 import PageWrapper from "@/components/animated-ui/page-wrapper";
+import { AppDescription, ApplicationSiteName } from "../utils/constants";
 
 export const revalidate = 30;
 
@@ -23,7 +24,7 @@ export async function generateMetadata(): Promise<Metadata> {
   const page = await client.getSingle("homepage");
 
   return {
-    title: page.data.title,
-    description: page.data.meta_description,
+    title: page.data.title || ApplicationSiteName,
+    description: page.data.meta_description || AppDescription,
   };
 }
