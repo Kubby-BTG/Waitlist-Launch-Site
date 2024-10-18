1c052a32eaa00b047d94ec63fd6220d0cfa113ae
Chris Uzoh <genbliz@gmail.com>
2024-10-18 16:21:39 +0100

    disable ban

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -118,14 +118,6 @@ export default async function RootLayout({
     );
   }
 
-  if (canRender) {
-    return (
-      <html lang="en">
-        <body>Nill</body>
-      </html>
-    );
-  }
-
   return (
     <html lang="en" className={"scroll-smooth"}>
       <body className={cn([DMSans.variable, TTBluescreens.variable, "font-sans"])}>
