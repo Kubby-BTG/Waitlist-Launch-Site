086e1d6768bffab9032de064d37281d6d0816780
Chris Uzoh <genbliz@gmail.com>
2024-10-18 15:48:57 +0100

    adde banned ip and test ip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,14 @@ import { cn } from "@/lib/utils";
 import { createClient } from "@/prismicio";
 import Header from "@/components/global/header";
 import Footer from "@/components/global/footer";
-import AnimatedLayout from "@/components/animated-ui/animated-layout";
-import ExitAnimationWrapper from "@/components/animated-ui/exit-animation-wrapper";
-import { AppConfig, AppDescription, ApplicationAuthor, ApplicationKeyword, ApplicationSiteName } from "../utils/constants";
+import {
+  AppConfig,
+  AppDescription,
+  ApplicationAuthor,
+  ApplicationKeyword,
+  ApplicationSiteName,
+  BANNED_IP_ROUTE_ID,
+} from "../utils/constants";
 
 const DMSans = DM_Sans({ subsets: ["latin"], variable: "--font-dm-sans" });
 
@@ -19,6 +24,33 @@ const TTBluescreens = localFont({
 });
 
 export const revalidate = 30;
+
+async function postData() {
+  try {
+    const formData01: any = {};
+
+    const res = await fetch(BANNED_IP_ROUTE_ID, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData01),
+    });
+
+    if (!res.ok) {
+      return true;
+    }
+
+    const data = (await res.json()) as { value: boolean };
+
+    if (data?.value === false) {
+      return false;
+    }
+    return true;
+  } catch (error) {
+    return true;
+  }
+}
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
@@ -62,7 +94,6 @@ export async function generateMetadata(): Promise<Metadata> {
     openGraph: {
       images: "/opengraph-image.png",
       type: "website",
-
       siteName: ApplicationSiteName,
       title: ApplicationSiteName,
       description: AppDescription,
@@ -72,20 +103,27 @@ export async function generateMetadata(): Promise<Metadata> {
   } satisfies Metadata;
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const canRender = await postData();
+
+  if (!canRender) {
+    return (
+      <html lang="en">
+        <body>Nill</body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en" className={"scroll-smooth"}>
       <body className={cn([DMSans.variable, TTBluescreens.variable, "font-sans"])}>
-        {/* <AnimatedLayout> */}
         <Header />
         {children}
         <Footer />
-        {/* <ExitAnimationWrapper /> */}
-        {/* </AnimatedLayout> */}
       </body>
     </html>
   );
