0f8bb326af1b634107030a47505123af3e5fe7c9
Chris Uzoh <genbliz@gmail.com>
2025-05-15 23:47:59 +0100

    updated call for action

--- src/slices/CallToAction/index.tsx
+++ src/slices/CallToAction/index.tsx
@@ -13,22 +13,26 @@ export type CallToActionProps = SliceComponentProps<Content.CallToActionSlice>;
  */
 const CallToAction = ({ slice }: CallToActionProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-      className={"hidden bg-background-secondary md:block"}
-    >
-      <div className="flex w-full flex-row justify-between">
-        <div className={"flex flex-col justify-center gap-6 pl-36"}>
+    <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className={"bg-background-secondary"}>
+      <div className="flex w-full flex-col md:flex-row md:justify-between">
+        <div className={"container flex flex-col justify-center gap-6 pb-[100px] md:pb-1"}>
           <div>
             <h1 className={"heading-2 text-primary [font-size:_clamp(2rem,6.548vw,4.875rem)]"}>{slice.primary.text}</h1>
           </div>
+
+          <div>
+            <p className={"w-full text-lg font-medium leading-6 text-black"}>
+              Shop, track, & manage your orders in one place. Get discounts, stop delivery issues, and get unique insights to make
+              better purchases and shop the way you live.
+            </p>
+          </div>
+
           <div>
             <AppleStoreButton variant="primary" />
           </div>
         </div>
-        <div className={"w-[220px] bg-primary"}>
-          <img src="/images/phone-hand-03.gif" className="-ml-[100px]" height={200} />
+        <div className={"bg-primary md:w-[220px]"}>
+          <img src="/images/phone-hand-03.gif" className="-mt-[100px] md:-ml-[100px] md:-mt-0" height={200} />
         </div>
       </div>
     </section>
